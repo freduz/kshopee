8a940539cb7acfbc2972deb4f4289779439f8599
Fredy Daniel <fredyd900@gmail.com>
2023-05-21 12:57:20 +0200

    all-initial-setup-done

---
+++ src/main/java/com/daniel/kshopee/KshopeeApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.kshopee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KshopeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KshopeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/kshopee/KshopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.kshopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KshopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
